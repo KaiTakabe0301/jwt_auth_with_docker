de371f28bb53e46fa0cfec62ea958382f83f3c9c
toefuVox <ph.c11.java.eo12@gmail.com>
2022-09-05 00:41:01 +0900

    routingを整理

--- src/api/routes/auth.js
+++ src/api/routes/auth.js
@@ -8,19 +8,7 @@ const saltRounds = 10;
 
 const jwt = require("jsonwebtoken");
 
-// ユーザ一覧
-router.get("/users", (req, res, next) => {
-  pool.query("SELECT * FROM users", (error, results) => {
-    if (error) {
-      throw error;
-    }
-    if (results) {
-      return res.status(200).json({
-        data: results.rows,
-      });
-    }
-  });
-});
+
 
 // ユーザ登録
 router.post("/auth/register", (req, res, next) => {
@@ -57,29 +45,6 @@ router.post("/auth/register", (req, res, next) => {
         }
       );
     });
-});
-
-router.delete("/delete/:id", (req, res, next) => {
-  const id = req.params.id;
-  pool.query("DELETE FROM users WHERE id = $1", [id], (error, results) => {
-    if (error) {
-      return res.status(500).json({
-        status: "500 Internal Server Error",
-        error: error,
-      });
-    }
-    if (results.rowCount === 0) {
-      return res.status(400).json({
-        status: "400 Bad Request",
-        message: "データが存在しません",
-      });
-    } else {
-      return res.status(200).json({
-        status: "success",
-        message: "データを削除しました",
-      });
-    }
-  });
 });
 
 router.post("/auth/login", (req, res, next) => {
--- src/api/routes/users.js
+++ src/api/routes/users.js
@@ -1,9 +1,44 @@
 var express = require('express');
 var router = express.Router();
 
+const pool = require("../../db/pool");
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// ユーザ一覧
+router.get("/", (req, res, next) => {
+  pool.query("SELECT * FROM users", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    if (results) {
+      return res.status(200).json({
+        data: results.rows,
+      });
+    }
+  });
+});
+
+router.delete("/delete/:id", (req, res, next) => {
+  const id = req.params.id;
+  pool.query("DELETE FROM users WHERE id = $1", [id], (error, results) => {
+    if (error) {
+      return res.status(500).json({
+        status: "500 Internal Server Error",
+        error: error,
+      });
+    }
+    if (results.rowCount === 0) {
+      return res.status(400).json({
+        status: "400 Bad Request",
+        message: "データが存在しません",
+      });
+    } else {
+      return res.status(200).json({
+        status: "success",
+        message: "データを削除しました",
+      });
+    }
+  });
 });
 
 module.exports = router;
